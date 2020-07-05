3a575de48b23923da441412f5be972d54290b13b
Anton Semenov <anta.semenov@gmail.com>
2020-07-05 10:12:12 +0500

    change workout sorting

--- src/selectors/workouts.ts
+++ src/selectors/workouts.ts
@@ -3,7 +3,7 @@ import { State } from 'types/'
 
 export const getWorkouts = createSelector(
   (state: State) => state.workouts,
-  (workoutsDictionary) => Object.values(workoutsDictionary),
+  (workoutsDictionary) => Object.values(workoutsDictionary).sortedByProperty((item) => item.id),
 )
 
 export const getWorkout = (state: State, id: number) => state.workouts[id]
